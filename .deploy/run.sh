dd12d028df921c8b58b4137c78c5efa5241cef52
Ryan Conrad <camalot@gmail.com>
2018-07-14 10:35:24 -0500

    try mapping

--- .deploy/run.sh
+++ .deploy/run.sh
@@ -56,11 +56,8 @@ docker pull "${DOCKER_REGISTRY}/${DOCKER_IMAGE}";
 echo -e "machine api.heroku.com\n\tlogin ${HEROKU_EMAIL}\n\tpassword ${HEROKU_AUTH}\n\n" >> ~/.netrc;
 echo -e "machine git.heroku.com\n\tlogin ${HEROKU_EMAIL}\n\tpassword ${HEROKU_AUTH}\n\n" >> ~/.netrc;
 
-cat ~/.netrc
-
-docker run wingrunr21/alpine-heroku-cli login;
-docker run wingrunr21/alpine-heroku-cli container:push web --app "${HEROKU_APP}";
-
+docker run wingrunr21/alpine-heroku-cli -v ~/:~/ container:push web --app "${HEROKU_APP}";
+docker run wingrunr21/alpine-heroku-cli -v ~/:~/ container:release web --app "${HEROKU_APP}";
 
 # docker run -d \
 # 	--user 0 \
